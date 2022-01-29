3777648b6bb9f46d96ede84a1920c1f9584c0b13
Giresh Dora <gireshkumar.dora@honeywell.com>
2022-01-29 18:34:10 +0530

    Initial Commit

---
+++ FloatingTextfiled/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FloatingTextfiled
+//
+//  Created by Giresh Dora on 29/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FloatingTextfiled/FloatingTextfiledApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextfiledApp.swift
+//  FloatingTextfiled
+//
+//  Created by Giresh Dora on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTextfiledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
